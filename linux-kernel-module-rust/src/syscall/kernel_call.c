63360bae3cc5a7515c7165d1b5d4fe2d57cc602b
DnailZ <dnailzb@outlook.com>
2020-06-25 10:46:15 +0000

    autosave

--- linux-kernel-module-rust/src/syscall/kernel_call.c
+++ linux-kernel-module-rust/src/syscall/kernel_call.c
@@ -4,7 +4,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/types.h>
-// #include <linux/syscalls.h>
 #include <linux/delay.h>
 #include <linux/sched.h>
 #include <linux/version.h>
