3b4e4702740913afe11c228e6f3e26089e9ea182
DnailZ <dnailzb@outlook.com>
2020-06-25 10:45:55 +0000

    autosave

--- linux-kernel-module-rust/make.rb
+++ linux-kernel-module-rust/make.rb
@@ -8,7 +8,19 @@ require "./syscall.rb"
 make do
     :orig_syscall .then do
         using dir('src/syscall') do
-            
+            File.open('orig.rs', 'w') do |f|
+                f.puts "use {"
+                f.puts "    super::*,"
+                f.puts "}"
+                
+                f.puts "mod cshim {"
+                f.puts "    extern \"C\" {"
+                $syscall.each do |k, v|
+                    
+                end
+                f.puts "    }"
+                f.puts "}"
+            end
         end
     end
 end
