ff71d929b70786801c3f50150783583b42861cc1
DnailZ <dnailzb@outlook.com>
2020-06-25 09:57:36 +0000

    autosave

---
+++ linux-kernel-module-rust/syscall.rb
@@ -0,0 +1,32 @@
+
+$syscalls = Hash[]
+
+File.readlines("../zCore/linux-syscall/src/lib.rs").each do |line|
+    m = line.match(/^\W*Sys::(\w+)/)
+    if m
+        $syscalls[m[1].downcase] = []
+    end
+end
+
+
+doc = File.open("syscall_table.html") { |f| Nokogiri::XML(f) }
+
+def parse_type(str)
+    name = str.match(/(\w*)$/)[1]
+    type = str[0..(str.length - name.length - 1)]
+    [name, type]
+end
+
+trs = doc.xpath("//tr")
+
+trs.each do |tr|
+    tds = tr.children
+    p tds[1]
+    if $syscalls[tds[1].content] != nil
+        tds[4..-1].each do |td|
+            if td.content != "-"
+                $syscalls[tds[1].content] += [parse_type(td.content)]
+            end
+        end
+    end
+end
