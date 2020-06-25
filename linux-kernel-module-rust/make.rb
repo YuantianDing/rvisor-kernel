b5e8ec3f4420c4eb9e54170d22f391170384c644
DnailZ <dnailzb@outlook.com>
2020-06-25 09:29:31 +0000

    autosave

--- linux-kernel-module-rust/make.rb
+++ linux-kernel-module-rust/make.rb
@@ -3,28 +3,30 @@
 require "ruby_make_script"
 require 'nokogiri'
 
-syscall_list = Hash[]
+syscalls = Hash[]
 File.readlines("../zCore/linux-syscall/src/lib.rs") do |line|
-    syscall_list[line.match(/^\W*Sys::(\w+)/)[1].downcase] = []
+    syscalls[line.match(/^\W*Sys::(\w+)/)[1].downcase] = []
 end
 
 doc = File.open("syscall_table.html") { |f| Nokogiri::XML(f) }
 
 def parse_type(str)
-    str.match(/(\w*)$/)
+    name = str.match(/(\w*)$/)[1]
+    type = str[0..str.length - name-length]
+    [name, type]
 end
 
-syscalls = Hash[]
 syscallxml = doc.xpath("//tr")
 syscallxml.each { |xml|
     tds = xml.xpath("//")
-    if syscall_list[tds[1]] != nil
+    if syscalls[tds[1]] != nil
         tds[4..-1].each { |td|
-            syscall_list[tds[1]] += [parse_type(td.content)]
+            syscalls[tds[1]] += [parse_type(td.content)]
         }
     end
 }
 
+puts syscalls
 
 make do
     :orig_syscall .then do
