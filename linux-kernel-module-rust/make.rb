262efa8ec02cf9e6c9388d2b77a8208c326cc7b1
DnailZ <dnailzb@outlook.com>
2020-06-25 08:20:38 +0000

    autosave

--- linux-kernel-module-rust/make.rb
+++ linux-kernel-module-rust/make.rb
@@ -3,23 +3,23 @@
 require "ruby_make_script"
 
 syscall = [
-    :open = [""
-    :getpid = [""
-    :openat = [""
-    :clone = [""
-    :fork = [""
-    :vfork = [""
-    :execve = [""
-    :execveat = [""
-    :chdir = [""
-    :getcwd = [""
-    :mknodat = [""
-    :mknod = [""
-    :mkdirat = [""
-    :mkdir = [""
-    :rmdir = [""
-    :stat = [""
-    :lstat = [""
+    :open = [""]
+    :getpid = [""]
+    :openat = [""]
+    :clone = [""]
+    :fork = [""]
+    :vfork = [""]
+    :execve = [""]
+    :execveat = [""]
+    :chdir = [""]
+    :getcwd = [""]
+    :mknodat = [""]
+    :mknod = [""]
+    :mkdirat = [""]
+    :mkdir = [""]
+    :rmdir = [""]
+    :stat = [""]
+    :lstat = [""]
 ]
 
 make do
