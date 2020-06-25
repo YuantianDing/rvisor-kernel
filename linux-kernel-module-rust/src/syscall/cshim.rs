1374422c74e9fa39ddbbf898443fc82c54543d28
DnailZ <dnailzb@outlook.com>
2020-06-25 07:54:49 +0000

    autosave

--- linux-kernel-module-rust/src/syscall/cshim.rs
+++ linux-kernel-module-rust/src/syscall/cshim.rs
@@ -1,8 +1,7 @@
-use crate::bindings::*;
+use crate::bindings;
 
 extern "C" {
     pub fn orig_getpid() -> c_long;
-
     fn protect_fs() -> bindings::mm_segment_t;
     fn release_fs(oldfs : bindings::mm_segment_t);
 }
