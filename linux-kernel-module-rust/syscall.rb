abbd32639bc990f0a066b5a32453200a3751fa78
DnailZ <dnailzb@outlook.com>
2020-06-25 10:20:50 +0000

    autosave

--- linux-kernel-module-rust/syscall.rb
+++ linux-kernel-module-rust/syscall.rb
@@ -2,7 +2,17 @@ require 'nokogiri'
 
 $syscalls = Hash[]
 
-$rust_type = Hash[]
+$rust_type = Hash[
+    "unsigned int "=>"missing",
+    "char *"=>"missing",
+    "size_t "=>"missing",
+    "const char *"=>"missing",
+    "int "=>"missing",
+    "umode_t "=>"missing",
+    "struct __old_kernel_stat *"=>"missing",
+    "off_t "=>"missing",
+    "?"=>"missing",
+    "unsigned long "=>"missing", "int"=>"missing", "const struct sigaction *"=>"missing", "struct sigaction *"=>"missing", "size_t"=>"missing", "sigset_t *"=>"missing", "loff_t "=>"missing", "const struct iovec *"=>"missing", "struct itimerval *"=>"missing", "off_t *"=>"missing", "unsigned "=>"missing", "int *"=>"missing", "const char *const *"=>"missing", "pid_t "=>"missing", "struct rusage *"=>"missing", "struct old_utsname *"=>"missing", "long "=>"missing", "uid_t "=>"missing", "gid_t "=>"missing", "gid_t *"=>"missing", "siginfo_t *"=>"missing", "const struct sigaltstack *"=>"missing", "struct sigaltstack *"=>"missing", "const char * "=>"missing", "struct statfs *"=>"missing", "void *"=>"missing", "u32 *"=>"missing", "u32 "=>"missing", "struct timespec *"=>"missing", "struct linux_dirent64 *"=>"missing", "clockid_t "=>"missing", "struct __kernel_timespec *"=>"missing", "struct stat *"=>"missing", "loff_t *"=>"missing"]
 
 File.readlines("../zCore/linux-syscall/src/lib.rs").each do |line|
     m = line.match(/^[\s\t]*Sys::(\w+)/)
