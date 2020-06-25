ef7bb9f133c1435347639c3678ba3a46c4d4ab48
DnailZ <dnailzb@outlook.com>
2020-06-25 11:13:09 +0000

    autosave

--- linux-kernel-module-rust/syscall.rb
+++ linux-kernel-module-rust/syscall.rb
@@ -43,10 +43,15 @@ end
 
 doc = File.open("syscall_table.html") { |f| Nokogiri::XML(f) }
 
+$name_num
 def parse_type(str)
     name = str.match(/(\w*)$/)[1]
     type = str[0..(str.length - name.length - 1)]
     # $rust_type[type] = 'missing'
+    $name_num += 1
+    if name == ""
+        name = "arg#{$name_num}"
+    end
     [name, type]
 end
 
@@ -56,6 +61,7 @@ trs.each do |tr|
     tds = tr.children
     if $syscalls[tds[1].content] != nil
         tds[4..-1].each do |td|
+            $name_num = 0
             if td.content != "-"
                 $syscalls[tds[1].content] += [parse_type(td.content)]
             end
