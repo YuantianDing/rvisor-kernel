2dc7cf3c36301396e33fdff1336dcc19a0be6637
DnailZ <dnailzb@outlook.com>
2020-06-25 10:46:46 +0000

    autosave

--- linux-kernel-module-rust/make.rb
+++ linux-kernel-module-rust/make.rb
@@ -3,6 +3,23 @@
 require "ruby_make_script"
 require "./syscall.rb"
 
+c_header = """
+#include \"hook_syscall.h\"
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/types.h>
+#include <linux/delay.h>
+#include <linux/sched.h>
+#include <linux/version.h>
+#include <linux/kallsyms.h>
+#include <linux/semaphore.h>
+#include <asm/cacheflush.h>
+#include <linux/bitops.h>
+#include <linux/sizes.h>
+#include <linux/byteorder/generic.h>
+#include <linux/preempt.h>
+"""
 
 
 make do
@@ -16,7 +33,7 @@ make do
                 f.puts "mod cshim {"
                 f.puts "    extern \"C\" {"
                 $syscall.each do |k, v|
-                    
+
                 end
                 f.puts "    }"
                 f.puts "}"
