b87aae4c59e535a638d76df0a0913c10dff2e01e
DnailZ <dnailzb@outlook.com>
2020-06-26 16:25:53 +0000

    autosave

--- linux-kernel-module-rust/make.rb
+++ linux-kernel-module-rust/make.rb
@@ -26,6 +26,9 @@ make do
     :build .from "orig_syscall" do
         r "cargo build"
     end
+    :update_raw do
+        
+    end
     :orig_syscall .then do
         cd 'src/syscall'
         File.open('orig.rs', 'w') do |f|
