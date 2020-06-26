9dd943f77e56efe26991d84c1de2f60492785803
DnailZ <dnailzb@outlook.com>
2020-06-26 16:32:50 +0000

    autosave

--- make.rb
+++ make.rb
@@ -4,7 +4,7 @@ require "ruby_make_script"
 make do
     :update_raw .then do
         rm "-rf", "target/debug/build/lkm-*"
-        use dir('linux-kernel-module-rust') do
+        using dir('linux-kernel-module-rust') do
             r "cargo build -p lkm --features bindgen"
         end
         cp "target/debug/build/lkm-*/out/bindings", "linux-kernel-module-rust/src/"
