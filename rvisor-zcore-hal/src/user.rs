b04528157328e90ce1ef3d3de117ce85681fe4cb
DnailZ <dnailzb@outlook.com>
2020-06-26 13:20:29 +0000

    autosave

--- rvisor-zcore-hal/src/user.rs
+++ rvisor-zcore-hal/src/user.rs
@@ -73,6 +73,7 @@ impl<T, P: Policy> UserPtr<T, P> {
     }
 
     pub fn add(&self, count: usize) -> Self {
+        trace!("UserPtr::add > UserPtr Calculated!")
         UserPtr {
             ptr: unsafe { self.ptr.add(count) },
             mark: PhantomData,
