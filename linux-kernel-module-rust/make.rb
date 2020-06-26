7db0b513976d215bf5749526ddd7db18e563d7e9
DnailZ <dnailzb@outlook.com>
2020-06-26 09:54:41 +0000

    autosave

--- linux-kernel-module-rust/make.rb
+++ linux-kernel-module-rust/make.rb
@@ -5,20 +5,6 @@ require "./syscall.rb"
 
 c_headers = "
 #include \"hook_syscall.h\"
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/types.h>
-#include <linux/delay.h>
-#include <linux/sched.h>
-#include <linux/version.h>
-#include <linux/kallsyms.h>
-#include <linux/semaphore.h>
-#include <asm/cacheflush.h>
-#include <linux/bitops.h>
-#include <linux/sizes.h>
-#include <linux/byteorder/generic.h>
-#include <linux/preempt.h>
-#include <linux/syscall.h>
 "
 
 def rust_pair(v)
