6ffde7d6d4c699f8f6769193b7f017b86e3346bc
DnailZ <dnailzb@outlook.com>
2020-06-27 11:35:05 +0000

    autosave

--- linux-kernel-module-rust/src/user_ptr.rs
+++ linux-kernel-module-rust/src/user_ptr.rs
@@ -205,18 +205,15 @@ impl UserSlicePtrWriter {
 use alloc::string::String;
 
 pub fn read_from_user(user_ptr : u64, max_length : usize) -> KernelResult<String> {
-    info!("read_from_user");
     let mut ret = String::from("                                                                                                                             ");
     unsafe {
         let slice = ret.as_mut_str();
         let i = bindings::strncpy_from_user(slice.as_mut_ptr(), user_ptr as *const u8, max_length as u64);
         ret.truncate(i as usize);
         if i < 0 {
-            info!("read_from_user: error {}", i);
             return Err(Error::from_kernel_errno(i));
         }
     }
-    info!("read_from_user: {}", ret);
     Ok(ret)
 }
 
