5ac18b6e961aa04cfd45d3902e56e06178d4a039
DnailZ <dnailzb@outlook.com>
2020-06-27 14:40:25 +0000

    autosave

--- rvisor-zcore-hal/src/user.rs
+++ rvisor-zcore-hal/src/user.rs
@@ -185,10 +185,11 @@ impl<P: Read> UserPtr<UserPtr<u8, P>, P> {
             } else { false }
         ).unwrap();
 
-        self.read_array(len)?
+        let vec = self.read_array(len)?
             .into_iter()
             .map(|ptr| ptr.read_cstring())
-            .collect()
+            .collect();
+        vec
     }
 }
 
