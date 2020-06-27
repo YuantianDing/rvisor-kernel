432639272685e7f1ddb82dabb3c6e4798d530413
DnailZ <dnailzb@outlook.com>
2020-06-27 14:38:14 +0000

    autosave

--- rvisor-zcore-hal/src/user.rs
+++ rvisor-zcore-hal/src/user.rs
@@ -186,7 +186,7 @@ impl<P: Read> UserPtr<UserPtr<u8, P>, P> {
         ).unwrap();
 
         let vec = self.read_array(len)?
-            // .into_iter()
+            .into_iter()
             .map(|ptr| ptr.read_cstring())
             .collect();
         Ok(vec)
