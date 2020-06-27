ebc55a816e61e48b14a0466b17a5cee0894e2ea9
DnailZ <dnailzb@outlook.com>
2020-06-27 06:48:45 +0000

    autosave

--- rvisor-zcore-hal/src/user.rs
+++ rvisor-zcore-hal/src/user.rs
@@ -134,7 +134,7 @@ impl<T, P: Read> UserPtr<T, P> {
         UserSlicePtr::new_ptr(self.ptr as u64, size)
             .map_err(|_| { Error::InvalidPointer })?
             .reader()
-            .read(data.as_mut_ptr() as * const u8 as &mut [u8])
+            .read(data.as_slice() as &mut [u8])
             .map_err(|_| { Error::InvalidPointer })?;
         Ok(data)
     }
