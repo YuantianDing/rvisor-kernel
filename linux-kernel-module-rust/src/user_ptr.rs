190b46a77914d500477bac703697ebb7c6a4772d
DnailZ <dnailzb@outlook.com>
2020-06-27 14:44:45 +0000

    autosave

--- linux-kernel-module-rust/src/user_ptr.rs
+++ linux-kernel-module-rust/src/user_ptr.rs
@@ -185,28 +185,7 @@ impl UserSlicePtrWriter {
         self.len() == 0
     }
 
-    // pub fn write(&mut self, data: &[u8]) -> error::KernelResult<()> {
-    //     if data.len() > self.1 || data.len() > u32::MAX as usize {
-    //         return Err(error::Error::EFAULT);
-    //     }
-    //     let res = unsafe {
-    //         bindings::_copy_to_user(
-    //             self.0,
-    //             data.as_ptr() as *const c_types::c_void,
-    //             data.len() as _,
-    //         )
-    //     };
-    //     if res != 0 {
-    //         return Err(error::Error::EFAULT);
-    //     }
-    //     // Since this is not a pointer to a valid object in our program,
-    //     // we cannot use `add`, which has C-style rules for defined
-    //     // behavior.
-    //     self.0 = self.0.wrapping_add(data.len());
-    //     self.1 -= data.len();
-    //     Ok(())
-    // }
-    pub fn write_any_type<T>(&mut self, data: T) -> error::KernelResult<()> {
+    pub fn write(&mut self, data: &[u8]) -> error::KernelResult<()> {
         if data.len() > self.1 || data.len() > u32::MAX as usize {
             return Err(error::Error::EFAULT);
         }
