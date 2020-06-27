0dbddee3839cf52cf1f53c151b4e3f9792feed03
DnailZ <dnailzb@outlook.com>
2020-06-27 14:57:40 +0000

    autosave

--- rvisor-zcore-hal/src/user.rs
+++ rvisor-zcore-hal/src/user.rs
@@ -159,13 +159,13 @@ impl<P: Read> UserPtr<u8, P> {
     // ! modified
     pub fn read_cstring(&self) -> Result<String> {
         const BUFFER_MAX: i32 = 100;
-        use lkm::Error::*
+        use lkm::Error;
         trace!("UserPtr::read_cstring");
         self.check()?;
         readstr_from_user(self.ptr as _, BUFFER_MAX as _).map_err(
             |e| match e {
-                EFAULT => Error::BufferTooSmall,
-                EINVAL => Error::InvalidUtf8,
+                Error::EFAULT => Error::BufferTooSmall,
+                Error::EINVAL => Error::InvalidUtf8,
             }
         )
     }
